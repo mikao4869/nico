ea29a42f044271b3675d8cae51bc7e2ccbf1501f
박서현 <s24005@gsm.hs.kr>
2025-01-07 23:39:44 +0900

    로그인하면 hello+  이름

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const loginForm=document.querySelector(".login-form");
-const loginInput=document.querySelector(".login-form input");
+const loginForm=document.querySelector("#login-form");
+const loginInput=document.querySelector("#login-form input");
+const Link=document.querySelector("a");
+const greeting=document.querySelector("#greeting");
+
+
+const HIDDEN_CLASSNAME="hidden";
 
 function onLoginSubmit(event)
 {
 event.preventDefault();
 console.log(loginInput.value);
+const username=loginInput.value;
+loginForm.classList.add(HIDDEN_CLASSNAME);
+console.log(username);
+greeting.innerText="Hello"+username;
+greeting.innerText=`Hello ${username}`;
 
+greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
-
+Link.addEventListener("click",handleLinkClick)
