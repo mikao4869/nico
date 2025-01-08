10f521c16e664e8b5b80222441e74b7093d81532
박서현 <s24005@gsm.hs.kr>
2025-01-08 23:51:52 +0900

    로그인 왼료

--- app.js
+++ app.js
@@ -5,18 +5,29 @@ const greeting=document.querySelector("#greeting");
 
 
 const HIDDEN_CLASSNAME="hidden";
+const USERNAME_KEY="username";
 
-function onLoginSubmit(event)
-{
-event.preventDefault();
-console.log(loginInput.value);
-loginForm.classList.add(HIDDEN_CLASSNAME);
-const username=loginInput.value;
-localStorage.setItem("username",username);
-
-greeting.innerText=`Hello ${username}`;
-
-greeting.classList.remove(HIDDEN_CLASSNAME);
+function onLoginSubmit(event){
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY,username);
+  paintGreetings();
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+function paintGreetings(){
+  const username=localStorage.getItem(USERNAME_KEY);
+  greeting.innerText=`Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+
+const savedUsername=localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername===null){ //만약에 username 없다면 form을 작성할수 있게 보여주라
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}
+else{
+  paintGreetings();
+}
