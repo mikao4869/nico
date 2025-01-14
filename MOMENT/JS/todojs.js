0402e3f0ec1c3e8f73a045a4575f3a76a578750e
박서현 <s24005@gsm.hs.kr>
2025-01-14 21:13:51 +0900

    localstorage에 id주는값

--- MOMENT/JS/todojs.js
+++ MOMENT/JS/todojs.js
@@ -17,8 +17,9 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id=newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -31,7 +32,11 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
+  const newTodoObj={
+    text:newTodo,
+    id:Date.now(),
+  }
+  toDos.push(newTodoObj);
   paintToDo(newTodo);
   saveToDos();
 }
